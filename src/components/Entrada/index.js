f31d0a2c37a3aea705f5878a52cffec5eb5f173d
Kethlyn <kethlynsaraa@gmail.com>
2022-05-06 15:28:47 -0300

    Tela Entrada: layout

--- src/components/Entrada/index.js
+++ src/components/Entrada/index.js
@@ -3,6 +3,7 @@ import { useState, useContext } from "react";
 import UserContext from "../../contexts/UserContext";
 import { useNavigate } from "react-router-dom";
 
+import styled from "styled-components";
 
 function Entrada() {
     const { token } = useContext(UserContext);
@@ -26,15 +27,62 @@ function Entrada() {
         }
     }
     return (
-        <>
-        <p>Nova Entrada</p>
-        <form>
-            <input type="text" placeholder="Valor" required onChange={(e) => setValor(e.target.value)}></input>
-            <input type="text" placeholder="Descrição" required onChange={(e) => setDescricao(e.target.value)}></input>
-            <button type="submit" onClick={salvarEntrada}>Salvar Entrada</button>
-        </form>
-        </>
+        <Contanier>
+            <h1>Nova Entrada</h1>
+            <form>
+                <input type="text" placeholder="Valor" required onChange={(e) => setValor(e.target.value)}></input>
+                <input type="text" placeholder="Descrição" required onChange={(e) => setDescricao(e.target.value)}></input>
+                <button type="submit" onClick={salvarEntrada}>Salvar Entrada</button>
+            </form>
+        </Contanier>
     );
 }
+
+const Contanier = styled.div`
+    display: flex;
+    flex-direction: column;
+    padding: 25px 24px 383px 24px;
+
+    h1 {
+        font-weight: 700;
+        font-size: 26px;
+        line-height: 31px;
+        color: #ffffff;
+    }
+
+    form {
+        display: flex;
+        flex-direction: column;
+        margin-top: 40px;
+    }
+
+    input {
+            height: 58px;
+            background: #FFFFFF;
+            border-radius: 5px;
+            border: none;
+            margin-bottom: 13px;
+            padding-left: 15px;
+    }
+
+    input::placeholder {
+        font-weight: 400;
+        font-size: 20px;
+        line-height: 23px;
+        color: #000000;
+    }
+
+    button {
+        height: 46px;
+        background-color: #A328D6;
+        border-radius: 5px;
+        border: none;
+        text-align: center;
+        font-weight: 700;
+        font-size: 20px;
+        line-height: 23px;
+        color: #ffffff;
+    }
+`;
 
 export default Entrada;
