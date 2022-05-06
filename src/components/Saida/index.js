9f4f57275e8721ae45102a7245c7edf3d99f0db8
Kethlyn <kethlynsaraa@gmail.com>
2022-05-06 15:31:29 -0300

    Tela Saída: layout

--- src/components/Saida/index.js
+++ src/components/Saida/index.js
@@ -3,6 +3,8 @@ import { useContext, useState } from "react";
 import UserContext from "../../contexts/UserContext";
 import { useNavigate  } from "react-router-dom";
 
+import styled from "styled-components";
+
 function Saida() {
   const { token } = useContext(UserContext);
   const [valor, setValor] = useState("");
@@ -26,15 +28,62 @@ function Saida() {
   }
 
   return (
-    <>
-      <p>Nova Saida</p>
+    <Contanier>
+      <h1>Nova Saída</h1>
       <form>
         <input type="text" placeholder="Valor" value={valor} required onChange={(e) => setValor(e.target.value)}></input>
         <input type="text" placeholder="Descrição" value={descricao} required onChange={(e) => setDescricao(e.target.value)}></input>
         <button type="submit" onClick={salvarSaida}>Salvar Saída</button>
       </form>
-    </>
+    </Contanier>
   );
 }
+
+const Contanier = styled.div`
+    display: flex;
+    flex-direction: column;
+    padding: 25px 24px 383px 24px;
+
+    h1 {
+        font-weight: 700;
+        font-size: 26px;
+        line-height: 31px;
+        color: #ffffff;
+    }
+
+    form {
+        display: flex;
+        flex-direction: column;
+        margin-top: 40px;
+    }
+
+    input {
+            height: 58px;
+            background: #FFFFFF;
+            border-radius: 5px;
+            border: none;
+            margin-bottom: 13px;
+            padding-left: 15px;
+    }
+
+    input::placeholder {
+        font-weight: 400;
+        font-size: 20px;
+        line-height: 23px;
+        color: #000000;
+    }
+
+    button {
+        height: 46px;
+        background-color: #A328D6;
+        border-radius: 5px;
+        border: none;
+        text-align: center;
+        font-weight: 700;
+        font-size: 20px;
+        line-height: 23px;
+        color: #ffffff;
+    }
+`;
 
 export default Saida;
